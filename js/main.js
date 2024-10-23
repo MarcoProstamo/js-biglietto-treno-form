a464d2453ed7d2b4cb6aa553dcf2c21708d5bcc2
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-23 14:47:26 +0200

    Add → Added Necessary IDs and Selecting Elements from DOM

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// # Selecting DOM Elements
+const FullnameEl = document.querySelector("#input-fullname");
+const tripLengthEl = document.querySelector("#input-trip-length");
+const ageEl = document.querySelector("#input-age");
+const submitEl = document.querySelector("#submit-button");
+const clearEl = document.querySelector("#clear-button");
+const displayEl = document.querySelector("#display");
+const userNameEl = document.querySelector("#user-name");
+const ticketPriceEl = document.querySelector("#ticket-price");
